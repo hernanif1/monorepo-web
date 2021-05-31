725324eb0d0d43738a92486f337b0a941c430466
Hernani Fernandes <hernanif1@gmail.com>
2021-05-31 14:38:58 +0200

    add component pages

---
+++ shared/components/src/pages/Login/Login.stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react';
+import { Login } from "./Login"
+
+export default {
+  title: 'Form/Login',
+  component: Login,
+} as Meta;
+
+const Template: Story<{}> = (args) => <Login {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+};
